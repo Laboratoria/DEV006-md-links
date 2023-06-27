5ef8e9fbfd72f8d7917e29fd6989131dc23837ca
Dul <dul31389@gmail.com>
2023-06-27 16:42:28 -0500

    funcion convertir ruta relativa a absoluta

--- funciones.js
+++ funciones.js
@@ -6,4 +6,27 @@ function isAbsolute(route) {
   const currentPath = path.isAbsolute(route);
   return currentPath;
 }
-console.log(isAbsolute('prueba\readmeprueba.md'));
+//console.log(isAbsolute('prueba\readmeprueba.md'));
+
+function isExist(route) {
+  return fs.existsSync(route)
+}
+
+
+//convertir ruta relativa a absoluta
+function toAbsolute(currentPath) {
+  let absolutePath = '';
+
+  if (path.isAbsolute(currentPath) && isExist(currentPath)) {
+      absolutePath = currentPath.replace(/\\/g,'/');
+  } else if (isExist(currentPath)){
+      absolutePath = path.resolve(currentPath).replace(/\\/g,'/');
+  } else {
+    absolutePath = "tu ruta no existe";
+  }
+
+  return absolutePath + ' absolute path';
+}
+console.log(toAbsolute('./prueba/readmeprueba.md'));
+
+

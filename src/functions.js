be8d1e3dfea64b1e717453aae0de50972a035023
Clau2210 <claudiauriascaudillo@gmail.com>
2023-07-03 20:45:32 -0600

    Tests parte II

--- src/functions.js
+++ src/functions.js
@@ -35,7 +35,6 @@ const readDirectory = (filePath) => {
             } else{
                 const arrayMd = [];
                 routes.forEach((file) =>{
-                    //const resultExt = path.extname(file);
                     const fileAbsolutePath = path.join(filePath, file);
                     if (fs.statSync(fileAbsolutePath).isFile() && validateMd(fileAbsolutePath)){
                         arrayMd.push(fileAbsolutePath);
@@ -60,16 +59,14 @@ const readTextFile = (filePath) => {
             if (error) {
                 reject(error);
             } else {
-                resolve(data);
-                console.log(data);
-                
+                resolve(data);  
             }
     });
 });
 };
 
 // Valida si la extensión es .md
-const validateMd = (filePath) => { // isMd
+const validateMd = (filePath) => { 
     const extension = path.extname(filePath);
     if (extension === '.md') {
         return true;
@@ -79,7 +76,6 @@ const validateMd = (filePath) => { // isMd
 };
 
 // Revisa si hay links dentro del archivo y los extrae 
-
 const extractLinks = (data, fileName) =>{
     const allLinks = [];
     const mdRender = MarkdownIt();
@@ -91,13 +87,10 @@ const extractLinks = (data, fileName) =>{
         const href = link.getAttribute('href');
         const text = link.textContent.slice(0, 50);
         if (href.startsWith('https://') || href.startsWith('http://')) {
-            //allLinks.push({ href, text, fileName});
             allLinks.push({ 
                 Ruta: fileName,
                 Texto: text, 
                 Link: href
-                //Codigo: null,
-                //Estado: null  
             });
         }
     });
@@ -107,12 +100,8 @@ const extractLinks = (data, fileName) =>{
 
 // Verifica si son vàlidos los links
 const verifyLinks = (links) => {
-    //console.log(links);
     const arrayPromise = links.map((link) => {
         return new Promise((resolve, reject) => {
-            //fetch(link.href)
-            //fetch(link.Link)
-            //return global.fetch(link.href)
             axios
                 .get(link.Link)
                 .then((response) =>{
@@ -127,12 +116,6 @@ const verifyLinks = (links) => {
                 })
                 .catch((error) =>{
                     const arrayResult = {
-                        // Ruta: link.fileName,
-                        // Texto: link.text,
-                        // Link:link.href,
-                        // //Codigo: error.name,
-                        // Codigo: error.name,
-                        // Estado: error.message, 
                         Ruta: link.Ruta,
                         Texto: link.Texto,
                         Link:link.Link,
@@ -140,24 +123,14 @@ const verifyLinks = (links) => {
                         Codigo: null,
                         Estado: 'No se pudo acceder al enlace', 
                     };
-                    resolve(arrayResult);
-                    //reject(result);
+                        resolve(arrayResult);
+                        //reject(arrayResult);
                 });
         });
     });
     
     return Promise.all(arrayPromise);
 };
-
-// const arrayLinks = [{
-//     href: 'ttps://nodejs.org/es/',
-//     text: 'Node.js',
-//     fileName: 'C:/Users/cuc22/Documents/Laboratoria/MD-Links/DEV006-md-links/src/pruebas/prueba2.md'},
-// {
-//     href: 'https://developers.google.com/v8/',
-//     text: 'motor de JavaScript V8 de Chrome',
-//     fileName: 'C:/Users/cuc22/Documents/Laboratoria/MD-Links/DEV006-md-links/src/pruebas/prueba2.md'}
-// ];
 
 module.exports = {
             validateFile,
--- src/index.js
+++ src/index.js
@@ -15,31 +15,20 @@ const   {
 function mdLinks(path,options){
     return new Promise(function (resolve, reject){
         const resolveIsAbsolute = isAbsolute(path) ? path : absolutePath(path);
-        console.log(resolveIsAbsolute,'absolute');
         if (validateFile(resolveIsAbsolute)){
-            //console.log(validateFile,'validatefile');
             if (isAFile(resolveIsAbsolute)){
-                console.log('Is a file');
                 const resultIsMd = validateMd(resolveIsAbsolute);
-                console.log(resultIsMd, 'Este One');
                 if(resultIsMd === false){
                     reject('Error: No es un archivo .md');
                     return;
                 }
                 readTextFile(resolveIsAbsolute)
-                //console.log(readTextFile,'result de readtextfile')
                 .then((result) => {
-                    console.log(result,'result');
                     const extract = extractLinks(result, resolveIsAbsolute);
-                    console.log(extract, 'Extracto de liiiinks');
                     if (options.validate === false){
                         resolve(extract);
-                        //return extract;
                     }    else{
-                        //resolve(verifyLinks(extract))
                         verifyLinks(extract)
-                        //return verifyLinks(extract);
-                   
                         .then((results) =>{
                             resolve(results)
                         })
@@ -48,26 +37,6 @@ function mdLinks(path,options){
                         })
                         .catch(reject);
                     }
-                    
-                            //  const errors = results.filter((result) => result.Codigo !== 200);
-                            //  if (errors.length > 0) {
-                            //      console.log('Los siguientes enlaces son los inválidos:');
-                            //      errors.forEach((error) => {
-                            //          console.log(`- Ruta: ${error.Ruta}`);
-                            //          console.log(`  Texto: ${error.Texto}`);
-                            //          console.log(`  Enlace: ${error.Link}`);
-                            //          console.log(`  Código: ${error.Codigo}`);
-                            //          console.log(`  Estado: ${error.Estado}`);
-                //                  });
-                //              }
-                //              resolve(errors);
-                //          })
-                //          .catch(reject);
-                //     }
-                // })
-                // .catch(reject);
-                // }
-
                 if (isADirectory(resolveIsAbsolute)){
                     readDirectory(resolveIsAbsolute)
                     .then((result)=>{
@@ -76,10 +45,8 @@ function mdLinks(path,options){
                             const extract = extractLinks(result, link);
                             if (options.validate === false){
                                 return extract;
-                                
                             } else {
                                 return verifyLinks(extract);
-                                console.log((verifyLinks(extract)), 'verifylinks');
                             }
                         })
                         .catch(reject)
@@ -88,7 +55,6 @@ function mdLinks(path,options){
                         .then ((results) =>{
                             const flatAllResults = results.flat();
                             resolve(flatAllResults);
-                            console.log(flatAllResults, 'Total de links del directorio');
                         })
                         .catch (reject);
                     })
@@ -100,12 +66,11 @@ function mdLinks(path,options){
             }
     });
 }
-const path = ('../src/pruebas/prueba1.md');
+//const path = ('../src/pruebas/prueba1.md');
 //const path = ('../src/pruebas/text.txt');
-//const path = ('../src/pruebas/');
+const path = ('../src/pruebas/');
 const options = {validate: true};
 const resultFunction = mdLinks(path, options);
-//console.log(resultFunction, '¡Resultado de la función mdLinks!');
 resultFunction
 .then(function (result) {
     console.log(result, 'Es este...')
